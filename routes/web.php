93d2d47193aed7706ed081f0599b97ca240d0bde
Raisya Ayudya Ananda <chaciww333@gamil.com>
2025-09-10 13:22:05 +0700

    membuat halaman dashboard

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Auth::routes();
+
+
+Route::group([
+    'middleware' => ['auth']
+], function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resource('/admin', App\Http\Controllers\AdminController::class);
 });
